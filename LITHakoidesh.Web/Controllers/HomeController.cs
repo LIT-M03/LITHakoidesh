137cc7fa8d1d285caac800d52d3982ea2f7e25ae
Avrumi Friedman <info@lakewoodprogramming.com>
2016-03-30 11:44:28 -0400

    Added web config transform

--- LITHakoidesh.Web/Controllers/HomeController.cs
+++ LITHakoidesh.Web/Controllers/HomeController.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LITHakoidesh.Data;
+using LITHakoidesh.Web.Models;
 
 namespace LITHakoidesh.Web.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
         public ActionResult Index()
         {
-            return View();
+            var repo = new VisitsRepository(Properties.Settings.Default.LITConStr);
+            repo.AddVisit(Request.UserHostAddress);
+            var vm = new HomePageViewModel
+            {
+                FiveMostFrequentIPs = repo.GetFiveMostFrequentIPs(),
+                TodayCount = repo.GetVisitCountForToday(),
+                MostPopularIP = repo.GetMostPopularIpAddress()
+            };
+            return View(vm);
         }
-
     }
+
+
 }
---
+++ LITHakoidesh.Web/Models/HomePageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LITHakoidesh.Data;
+
+namespace LITHakoidesh.Web.Models
+{
+    public class HomePageViewModel
+    {
+        public IEnumerable<GetFiveMostFrequentIPsResult> FiveMostFrequentIPs { get; set; }
+        public int TodayCount { get; set; }
+        public string MostPopularIP { get; set; }
+    }
+}
